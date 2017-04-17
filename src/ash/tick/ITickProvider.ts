7555dca054953987d32f3a79cb673fd24a28538b
Grzegorz Michlicki <grzegorz@michlicki.pl>
2017-04-17 16:21:12 +0200

    Better ITickProvider interface definition

--- src/ash/tick/ITickProvider.ts
+++ src/ash/tick/ITickProvider.ts
@@ -3,12 +3,11 @@
  * to act as the heartbeat for the engine. It has methods to start and stop the tick and
  * to add and remove listeners for the tick.
  */
-export interface ITickProvider
-{
+export interface ITickProvider {
     playing:boolean;
-    add( listener:Function ) : void;
-    remove( listener:Function ) : void;
-    start() : void;
-    stop() : void;
+    add( listener:( delta:number ) => void ):void;
+    remove( listener:( delta:number ) => void ):void;
+    start():void;
+    stop():void;
 }
 
