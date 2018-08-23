4ae3e7011c52a1cd699b736f0bf859e061c9ed42
Grzegorz Michlicki <grzegorz@michlicki.pl>
2018-08-24 00:33:56 +0200

    Added ES5 build. Changed extension of ES6 module to .mjs. Added unminified versions of all builds.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,61 @@
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: 'ash/index.ts',
-  output: [
-    { format: 'umd', file: 'dist/ash.ts.umd.js', name: 'ash-ts' },
-    { format: 'esm', file: 'dist/ash.ts.esm.js', name: 'ash-ts' }
-  ],
-  plugins: [
-    typescript({ tsconfig: 'ash/tsconfig.json', useTsconfigDeclarationDir: true }),
-    terser()
-  ]
+const typescriptConfig = {
+  tsconfig: 'ash/tsconfig.json',
+  useTsconfigDeclarationDir: true
 };
+
+const tsconfigOverride = {
+  compilerOptions: {
+    target: 'es5',
+    downlevelIteration: true,
+    importHelpers: true,
+    lib: [
+      'dom',
+      'es5',
+      'es2015.collection',
+      'es2015.iterable'
+    ]
+  }
+};
+
+export default [
+  {
+    input: 'ash/index.ts',
+    output: [
+      { format: 'umd', file: 'dist/ash.ts.js', name: 'ash.ts' },
+      { format: 'esm', file: 'dist/ash.ts.mjs', name: 'ash.ts' }
+    ],
+    plugins: [
+      typescript(typescriptConfig)
+    ]
+  }, {
+    input: 'ash/index.ts',
+    output: [
+      { format: 'umd', file: 'dist/ash.ts.min.js', name: 'ash.ts' },
+      { format: 'esm', file: 'dist/ash.ts.min.mjs', name: 'ash.ts' }
+    ],
+    plugins: [
+      typescript(typescriptConfig),
+      terser()
+    ]
+  }, {
+    input: 'ash/index.ts',
+    output: [
+      { format: 'umd', file: 'dist/ash.ts.es5.js', name: 'ash.ts' }
+    ],
+    plugins: [
+      typescript({ ...typescriptConfig, tsconfigOverride }),
+    ]
+  }, {
+    input: 'ash/index.ts',
+    output: [
+      { format: 'umd', file: 'dist/ash.ts.es5.min.js', name: 'ash.ts' }
+    ],
+    plugins: [
+      typescript({ ...typescriptConfig, tsconfigOverride }),
+      terser()
+    ]
+  }
+];
