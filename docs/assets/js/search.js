aeed0047df1615fc49b754dfae1b5814616362a3
Grzegorz Michlicki <grzegorz@michlicki.pl>
2019-03-10 17:25:24 +0100

    Removing docs from repo

--- docs/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"globals.html#classtype.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"ClassType"},{"id":1,"kind":65536,"name":"__type","url":"globals.html#nodeclasstype.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"NodeClassType"}]};
