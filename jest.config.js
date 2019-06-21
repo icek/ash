5acc5a2e0cc814f67597a9268e57bfe2dd846a9c
Grzegorz Michlicki <grzegorz@michlicki.pl>
2019-06-21 17:39:28 +0200

    [Lerna] Moving jest config into it's own file

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['ash', 'core', 'fsm', 'signals', 'tick', 'tools']
+    .reduce((roots, name) => roots.concat(['src', 'tests'].map(dir => `<rootDir>/packages/${name}/${dir}`)), []),
+  transform: { '^.+\.ts$': 'ts-jest' },
+  testRegex: '/tests/.*\.spec\.ts$',
+  moduleFileExtensions: ['ts', 'js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['packages/**/src/*.ts'],
+};
