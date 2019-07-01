1f6694906b47433f67d8a04c3ac3243a44c012b7
Grzegorz Michlicki <grzegorz@michlicki.pl>
2019-07-01 17:31:55 +0200

    Fixed tests to be run on each package separately.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  roots: ['ash', 'core', 'fsm', 'signals', 'tick', 'tools']
-    .reduce((roots, name) => roots.concat(['src', 'tests'].map(dir => `<rootDir>/packages/${name}/${dir}`)), []),
-  transform: { '^.+\.ts$': 'ts-jest' },
-  testRegex: '/tests/.*\.spec\.ts$',
-  moduleFileExtensions: ['ts', 'js'],
+  projects: ['ash', 'core', 'fsm', 'signals', 'tick', 'tools'].map(dir => ({
+    roots: [
+      `<rootDir>/packages/${dir}/src`,
+      `<rootDir>/packages/${dir}/tests`,
+    ],
+    transform: { '^.+\.ts$': 'ts-jest' },
+    testRegex: '/tests/.*\.spec\.ts$',
+    moduleFileExtensions: ['ts', 'js'],
+  })),
   collectCoverageFrom: ['packages/**/src/*.ts'],
 };
