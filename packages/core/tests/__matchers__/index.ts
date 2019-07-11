7e144cb3a27d3dd89a8266ae1601a97fd97091df
Grzegorz Michlicki <grzegorz@michlicki.pl>
2019-07-11 16:55:57 +0200

    Lint fix

--- packages/core/tests/__matchers__/index.ts
+++ packages/core/tests/__matchers__/index.ts
@@ -4,7 +4,7 @@ import { toBeNodeList } from './toBeNodeList';
 expect.extend({ toBeNodeList });
 
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
+  // eslint-disable-next-line @typescript-eslint/no-namespace,no-redeclare
   namespace jest {
     interface Matchers<R> {
       toBeNodeList<TNode extends Node>(...expected:TNode[]):R;
