a146ecf93eae4d34874213a7ada1c851065bf10b
Grzegorz Michlicki <grzegorz@michlicki.pl>
2019-07-11 17:15:03 +0200

    Added things to exports that are required to implement custom io codecs.

--- packages/ash/src/index.ts
+++ packages/ash/src/index.ts
@@ -1,6 +1,16 @@
 import { ClassType, ComponentMatchingFamily, Engine, Entity, Family, keep, Node, NodeClassType, NodeList, NodePool, System } from '@ash.ts/core';
 import { EngineStateMachine, EntityStateMachine } from '@ash.ts/fsm';
-import { EngineCodec, JsonEngineCodec, ObjectCodec, ObjectEngineCodec } from '@ash.ts/io';
+import {
+  CodecManager,
+  EncodedComponent,
+  EncodedData,
+  EncodedEntity,
+  EncodedObject,
+  EngineCodec,
+  JsonEngineCodec,
+  ObjectCodec,
+  ObjectEngineCodec,
+} from '@ash.ts/io';
 import { Signal0, Signal1, Signal2, Signal3 } from '@ash.ts/signals';
 import { IntervalTickProvider, RAFTickProvider, TickProvider } from '@ash.ts/tick';
 import { ComponentPool, ListIteratingSystem } from '@ash.ts/tools';
@@ -25,6 +35,11 @@ export {
   ObjectCodec,
   JsonEngineCodec,
   ObjectEngineCodec,
+  EncodedData,
+  EncodedEntity,
+  EncodedComponent,
+  EncodedObject,
+  CodecManager,
   EntityStateMachine,
   EngineStateMachine,
   TickProvider,
--- packages/io/src/index.ts
+++ packages/io/src/index.ts
@@ -2,3 +2,5 @@ export { EngineCodec } from './enginecodecs/EngineCodec';
 export { ObjectCodec } from './objectcodecs/ObjectCodec';
 export { JsonEngineCodec } from './enginecodecs/JsonEngineCodec';
 export { ObjectEngineCodec } from './enginecodecs/ObjectEngineCodec';
+export { EncodedData, EncodedEntity, EncodedComponent, EncodedObject } from './enginecodecs/EncodedData';
+export { CodecManager } from './objectcodecs/CodecManager';
