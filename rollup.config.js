360c01c5dc9692ed3e1df6aac4e74db182bc6629
Grzegorz Michlicki <grzegorz@michlicki.pl>
2019-07-20 10:37:50 +0200

    Build config in more es6 way

--- rollup.config.js
+++ rollup.config.js
@@ -4,61 +4,49 @@ import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
-export default [
-  'signals',
-  'core',
-  'fsm',
-  'tick',
-  'tools',
-  'io',
-  'ash',
-].reduce((config, name) => {
+export default ['signals', 'core', 'fsm', 'tick', 'tools', 'io', 'ash'].reduce((config, name) => {
+  const root = `packages/${name}`;
+  const input = `${root}/src/index.ts`;
+  const filePath = `${root}/dist/${name}`;
   const plugins = [
     resolve(),
     typescript({
       useTsconfigDeclarationDir: true,
       clean: true,
       verbosity: 1,
-      tsconfig: `./packages/${name}/tsconfig.json`,
+      tsconfig: `${root}/tsconfig.json`,
       tsconfigOverride: {
         include: ['./src/'],
       },
     }),
   ];
-
-  const minPlugins = plugins.concat([
+  const minPlugins = [
+    ...plugins,
     terser({
       keep_classnames: true,
       keep_fnames: true,
     }),
-  ]);
+  ];
 
-  const root = `packages/${name}`;
-  const input = `${root}/src/index.ts`;
-
-  config.push({
-    input,
-    plugins,
-    output: [
-      { format: 'umd', file: `${root}/dist/${name}.js`, name },
-      { format: 'esm', file: `${root}/dist/${name}.mjs` },
-    ],
-  });
-
-  config.push({
-    input,
-    plugins: minPlugins,
-    output: [
-      { format: 'umd', file: `${root}/dist/${name}.min.js`, name },
-      { format: 'esm', file: `${root}/dist/${name}.min.mjs` },
-    ],
-  });
-
-  config.push({
-    input: `${root}/dist/types/index.d.ts`,
-    plugins: [dts()],
-    output: { file: `${root}/dist/${name}.d.ts`, format: 'es' },
-  });
-
-  return config;
+  return [
+    ...config,
+    {
+      input,
+      plugins,
+      output: [
+        { format: 'umd', file: `${filePath}.js`, name },
+        { format: 'esm', file: `${filePath}.mjs` },
+      ],
+    }, {
+      input,
+      plugins: minPlugins,
+      output: [
+        { format: 'umd', file: `${filePath}.min.js`, name },
+        { format: 'esm', file: `${filePath}.min.mjs` },
+      ],
+    }, {
+      input: `${root}/dist/types/index.d.ts`,
+      plugins: [dts()],
+      output: { file: `${filePath}.d.ts`, format: 'es' },
+    }];
 }, []);
