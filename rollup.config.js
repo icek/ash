396196959c4b1d1ddfa32cb3633b7987e8a75cf5
Grzegorz Michlicki <grzegorz@michlicki.pl>
2020-04-24 09:30:43 +0200

    Simplified the development environment a tiny bit based on changes made by @Andresa88. Removed tsconfig.json inside packages. Optimized rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,60 +1,51 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import dts from 'rollup-plugin-dts';
 import resolve from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
-const packages = ['signals', 'core', 'fsm', 'tick', 'tools', 'io', 'ash'];
+const bundlePackageName = 'ash';
+const packages = ['signals', 'core', 'fsm', 'tick', 'tools', 'io', bundlePackageName];
 export default packages.reduce((config, packageName) => {
+  const ashGlobal = 'ASH';
   const root = `packages/${packageName}`;
-  const input = `${root}/src/index.ts`;
   const filePath = `${root}/dist/${packageName}`;
-  const name = packageName === 'ash' ? 'ash' : `ash.${packageName}`;
-  const globals = packageName === 'ash' ? undefined : pkg => pkg.replace(/^@ash\.ts\/(.*)$/g, `ash.$1`);
-  const external = packageName === 'ash' ? undefined : packages.map(pkg => `@ash.ts/${pkg}`);
-  const plugins = [
-    resolve({
-      modulesOnly: true,
-    }),
-    typescript({
-      useTsconfigDeclarationDir: true,
-      clean: true,
-      verbosity: 1,
-      tsconfig: `${root}/tsconfig.json`,
-      tsconfigOverride: {
-        include: ['./src/'],
-      },
-    }),
-  ];
-  const minPlugins = [
-    ...plugins,
-    terser({
-      keep_classnames: true,
-      keep_fnames: true,
-    }),
-  ];
+  const isBundle = packageName === bundlePackageName;
+  const name = isBundle ? ashGlobal : `${ashGlobal}.${packageName}`;
+  const globals = isBundle ? undefined : pkg => pkg.replace(/^@ash\.ts\/(.*)$/g, `${ashGlobal}.$1`);
+  const external = isBundle ? undefined : pkg => pkg.startsWith('@ash.ts/');
 
   return [
     ...config,
     {
-      input,
-      plugins,
+      input: `${root}/src/index.ts`,
+      plugins: [
+        resolve({ modulesOnly: true }),
+        typescript({
+          useTsconfigDeclarationDir: true,
+          clean: true,
+          verbosity: 1,
+          // tsconfig: 'tsconfig.json',
+          tsconfigOverride: {
+            include: [`${root}/src/`],
+            compilerOptions: {
+              baseUrl: root,
+              declarationDir: `${root}/dist/types`,
+            },
+          },
+        }),
+        terser({ keep_classnames: true, keep_fnames: true, include: [/^.+\.min\.m?js$/] }),
+      ],
       external,
       output: [
         { format: 'umd', file: `${filePath}.js`, globals, name },
-        { format: 'esm', file: `${filePath}.mjs`, globals },
-      ],
-    }, {
-      input,
-      plugins: minPlugins,
-      external,
-      output: [
         { format: 'umd', file: `${filePath}.min.js`, globals, name },
-        { format: 'esm', file: `${filePath}.min.mjs`, globals },
+        { format: 'es', file: `${filePath}.mjs` },
+        { format: 'es', file: `${filePath}.min.mjs` },
       ],
     }, {
       input: `${root}/dist/types/index.d.ts`,
       plugins: [dts()],
-      output: { file: `${filePath}.d.ts`, format: 'es' },
+      output: { format: 'es', file: `${filePath}.d.ts` },
     }];
 }, []);
